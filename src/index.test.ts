8b10587339968ceb8d080a4f093ed0550357c387
Michael van Tellingen <m.vantellingen@labdigital.nl>
2024-11-09 13:11:10 +0100

    feat: support storing null values

--- src/index.test.ts
+++ src/index.test.ts
@@ -71,11 +71,16 @@ describe('Test keyv store', () => {
 
   const fetchItemsBySlugUncached = async (
     keys: readonly MyKey[]
-  ): Promise<(MyValue | Error)[]> =>
-    keys.map((key) => ({
-      slug: key.slug,
-      name: key.slug,
-    }))
+  ): Promise<(MyValue | null | Error)[]> =>
+    keys.map((key) => {
+      if (key.slug.startsWith('null-')) {
+        return null
+      }
+      return {
+        slug: key.slug,
+        name: key.slug,
+      }
+    })
 
   const loader = new DataLoader<MyKey, any>(fetchItemsBySlug, {
     maxBatchSize: 50,
@@ -133,6 +138,73 @@ describe('Test keyv store', () => {
 
       expect(cachedValue[2].slug).toBe('test-2')
       expect(cachedValue[2].name).toBe('test-2')
+    }
+  })
+
+  it('Cache missing product', async () => {
+    const value = await loader.load({
+      id: '1',
+      slug: 'test-1',
+    })
+    expect(value.slug).toBe('test-1')
+    expect(value.name).toBe('test-1')
+
+    // This should be one cache hit
+    {
+      const cachedValue = await loader.loadMany([
+        {
+          id: '1',
+          slug: 'test-1',
+        },
+        {
+          id: '2',
+          slug: 'test-2',
+        },
+      ])
+      expect(cachedValue[0].slug).toBe('test-1')
+      expect(cachedValue[0].name).toBe('test-1')
+
+      expect(cachedValue[1].slug).toBe('test-2')
+      expect(cachedValue[1].name).toBe('test-2')
+    }
+
+    // This should be two cache hits
+    {
+      const cachedValue = await loader.loadMany([
+        {
+          id: '1',
+          slug: 'test-1',
+        },
+        {
+          id: '3',
+          slug: 'test-3',
+        },
+        {
+          id: '2',
+          slug: 'test-2',
+        },
+        {
+          id: '4',
+          slug: 'null-1',
+        },
+      ])
+
+      expect(cachedValue[0].slug).toBe('test-1')
+      expect(cachedValue[0].name).toBe('test-1')
+
+      expect(cachedValue[1].slug).toBe('test-3')
+      expect(cachedValue[1].name).toBe('test-3')
+
+      expect(cachedValue[2].slug).toBe('test-2')
+      expect(cachedValue[2].name).toBe('test-2')
+
+      expect(cachedValue[3]).toBe(null)
+
+      const storedNull = await store.get('item-data:4:id:null-1')
+      expect(storedNull).toBeNull()
+
+      const storedUndefined = await store.get('item-data:5:id:null-1')
+      expect(storedUndefined).toBeUndefined()
     }
   })
 })
