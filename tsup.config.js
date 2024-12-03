ae2ab300b66b0c0c59fd3ce9404053993133f683
Michael van Tellingen <m.vantellingen@labdigital.nl>
2024-12-03 11:27:17 +0100

    style: update to biomejs

--- tsup.config.js
+++ tsup.config.js
@@ -1,9 +1,9 @@
 export default {
-  entry: ["src/index.ts"],
-  clean: true,
-  splitting: false,
-  dts: true,
-  sourcemap: true,
-  format: ["cjs", "esm", "iife"],
-  outDir: "dist",
+	entry: ["src/index.ts"],
+	clean: true,
+	splitting: false,
+	dts: true,
+	sourcemap: true,
+	format: ["cjs", "esm", "iife"],
+	outDir: "dist",
 };
